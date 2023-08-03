40152db8b00200f36b45fcc3fb967befd580c903
SmokeyNoom <smokeynoom@gmail.com>
2023-08-03 21:32:30 +0800

    dark mode

--- website/script.js
+++ website/script.js
@@ -4,14 +4,14 @@ function login() {
 
       // Perform any client-side validation you want here (e.g., check if fields are not empty).
       const resp2 = fetch("http://127.0.0.1:8080/users/auth", {
-            method:"POST",
-            mode:"no-cors",
+            method: "POST",
+            mode: "no-cors",
             headers: {
                   'Content-Type': 'application/json;charset=utf-8'
             },
-            body:JSON.stringify({
-                  "email":email,
-                  "password":password
+            body: JSON.stringify({
+                  "email": email,
+                  "password": password
             })
       });
       resp2.then(response => response.json()).catch(error => console.error(error));
@@ -33,16 +33,16 @@ function signUp() {
       const newPassword = document.getElementById('newPassword').value
 
       // Perform any client-side validation you want here (e.g., check if fields are not empty).
-      const resp = fetch("http://127.0.0.1:8080/users",{
-            method:"POST",
-            mode:"no-cors",
-            headers:{
+      const resp = fetch("http://127.0.0.1:8080/users", {
+            method: "POST",
+            mode: "no-cors",
+            headers: {
                   'Content-Type': 'application/json;charset=utf-8'
             },
-            body:JSON.stringify({
-                  "username":newUsername,
-                  "email":newEmail,
-                  "password":newPassword
+            body: JSON.stringify({
+                  "username": newUsername,
+                  "email": newEmail,
+                  "password": newPassword
             })
       });
       resp.then(response => response.json()).catch(error => console.error(error))
@@ -52,7 +52,7 @@ function signUp() {
       //      ).innerText = response.reason;
       //      return false;
       //};
-      
+
       // For the sake of this example, let's just show a success message.
       document.getElementById(
             'signup-success'
@@ -64,3 +64,19 @@ function onMessage() {
       const message = document.getElementById
 
 }
+
+const toggleDarkModeButton = document.getElementById('toggleDarkMode');
+const pageContainer = document.getElementById('page-container');
+let isDarkMode = false;
+
+function toggleDarkMode() {
+  isDarkMode = !isDarkMode;
+
+  if (isDarkMode) {
+    pageContainer.classList.add('dark-mode');
+  } else {
+    pageContainer.classList.remove('dark-mode');
+  }
+}
+
+toggleDarkModeButton.addEventListener('click', toggleDarkMode);
